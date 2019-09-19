9a77a1c7fa5581710ed2e6d5342808c74846184d
Bryan Moffit <moffit@jlab.org>
2019-09-18 23:07:59 -0400

    Some initial routines to get variables from config file. Will fill them in when I understand the GI_Config usage.

--- decconfig.cc
+++ decconfig.cc
@@ -10,12 +10,13 @@
 
 
 #include <decconfig.hh>
+#include <string>
 
-decconfig::decconfig(std::string filename)
+decconfig::decconfig(std::string filename = "decode.cfg")
 {
-  rocnum = -1;
+  current_rocnum = -1;
   cfg = new GI_Config();
-  cfg->parseFile("decode.cfg");
+  cfg->parseFile(filename);
 }
 
 decconfig::~decconfig()
@@ -23,7 +24,136 @@ decconfig::~decconfig()
   delete cfg;
 }
 
-decconfig::SetRocNumber(int rocnum)
+bool
+decconfig::show_block_header(int mpd)
 {
-  current_rocnum = roc_num;
+  return true;
+}
+
+bool
+decconfig::show_block_trailer(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::show_event_header(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::show_trigger_time(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::show_apv_header(int mpd, int apv)
+{
+  return true;
+}
+
+bool
+decconfig::show_apv_data(int mpd, int apv)
+{
+  return true;
+}
+
+bool
+decconfig::show_apv_trailer(int mpd, int apv)
+{
+  return true;
+}
+
+bool
+decconfig::show_event_trailer(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::show_filler_word(int mpd)
+{
+  return true;
+}
+
+
+bool
+decconfig::check_apvmask(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::check_napv(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::check_event_count(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::check_ndata(int mpd, int apv)
+{
+  return true;
+}
+
+bool
+decconfig::check_sample_count(int mpd, int apv)
+{
+  return true;
+}
+
+bool
+decconfig::check_frame_count(int mpd, int apv)
+{
+  return true;
+}
+
+bool
+decconfig::check_baseline_value(int mpd, int apv)
+{
+  return true;
+}
+
+bool
+decconfig::check_word_count(int mpd, int apv)
+{
+  return true;
+}
+
+bool
+decconfig::check_n_words_in_event(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::check_n_words_in_block(int mpd)
+{
+  return true;
+}
+
+bool
+decconfig::check_data_count(int mpd, int apv)
+{
+  return true;
+}
+
+
+int
+decconfig::minimum_baseline()
+{
+  return 1;
+}
+
+int
+decconfig::maximum_baseline()
+{
+  return 1;
 }
--- decconfig.hh
+++ decconfig.hh
@@ -2,13 +2,47 @@
 //    Class to handle configuring the output of mpddata
 //
 #include <GI_Config.h>
-
+#include <stdint.h>
 
 class decconfig
 {
+public:
+  decconfig(std::string filename);
+  ~decconfig();
+
+  void set_roc_number(int rocnum)
+  {
+    current_rocnum = rocnum;
+  }
+
+  bool show_block_header(int mpd);
+  bool show_block_trailer(int mpd);
+  bool show_event_header(int mpd);
+  bool show_trigger_time(int mpd);
+  bool show_apv_header(int mpd, int apv);
+  bool show_apv_data(int mpd, int apv);
+  bool show_apv_trailer(int mpd, int apv);
+  bool show_event_trailer(int mpd);
+  bool show_filler_word(int mpd);
+
+  bool check_apvmask(int mpd);
+  bool check_napv(int mpd);
+  bool check_event_count(int mpd);
+  bool check_ndata(int mpd, int apv);
+  bool check_sample_count(int mpd, int apv);
+  bool check_frame_count(int mpd, int apv);
+  bool check_baseline_value(int mpd, int apv);
+  bool check_word_count(int mpd, int apv);
+  bool check_n_words_in_event(int mpd);
+  bool check_n_words_in_block(int mpd);
+  bool check_data_count(int mpd, int apv);
+
+  int minimum_baseline();
+  int maximum_baseline();
 
 private:
   GI_Config *cfg;
-  uint8_t rocnum;
+  uint8_t current_rocnum;
+
 
 };
