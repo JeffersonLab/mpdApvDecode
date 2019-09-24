ca5a927b1c251c5f859366780e1df361ef6e062e
Bryan Moffit <moffit@jlab.org>
2019-09-24 14:06:20 -0400

    Working procedure for finding ROC banks and uint32 data.

--- main.cc
+++ main.cc
@@ -133,8 +133,7 @@ main(int argc, char **argv)
       }
 
 
-    int32_t rocnum = 4, mpdtag = 10, mpdnum = 0;
-    while (chan->readAlloc((uint32_t **) & buffer, &blen))
+    while (chan->readAlloc((uint32_t **) &buffer, &blen))
       {
 	uint64_t eventNumber = 0;
 	uint64_t eventTimestamp = 0;
@@ -142,14 +141,36 @@ main(int argc, char **argv)
 	const uint64_t *d64;
 	const uint32_t *d32;
 	const uint16_t *d16;
-	evioBankIndex bi(buffer);
+
+	evioBankIndex ebi(buffer,1);
+
+	bankIndexMap::const_iterator b_iter;
+
+	// Get the tag numbers that are not FF50 or FF21
+	// These should be the ROC banks.
+	bankIndex bi_roc;
+	map <int, bankIndex> rocBiMap;
+	for(b_iter = ebi.tagNumMap.begin(); b_iter != ebi.tagNumMap.end();
+	    b_iter++)
+	  {
+	    if((b_iter->first.getTag() != 0xFF50) &&
+	       (b_iter->first.getTag() != 0xFF21))
+	      {
+#define DEBUG_BANKS
+#ifdef DEBUG_BANKS
+		cout << "In bi: tag = 0x" << hex << b_iter->first.getTag()
+		     << " type = 0x" << hex << b_iter->second.contentType << endl;
+#endif
+		rocBiMap.insert( pair <int, bankIndex>(b_iter->first.getTag(), b_iter->second) );
+		bi_roc = b_iter->second;
+	      }
+	  }
 
 	int len;
-
 	if (version < 4)
 	  {
 	    tn = evioDictEntry(0xC000, 0);
-	    d32 = bi.getData < uint32_t > (tn, &len);
+	    d32 = ebi.getData < uint32_t > (tn, &len);
 	    if (d32 != NULL)
 	      {
 		eventNumber = (uint32_t) d32[0];
@@ -158,47 +179,54 @@ main(int argc, char **argv)
 	  }
 	else
 	  {
-	    tn = evioDictEntry(0x2,	// Eventnumber/timestamp bank
-			       0,	// num
-			       0,	// tagend
-			       true,	// hasparent
-			       0xFF21,	// parentTag
-			       1,	// parentNum
-			       0,	// parentTagEnd
-			       EVIO_ULONG64, false, "", "");
-	    d64 = bi.getData < uint64_t > (tn, &len);
+	    tn = evioDictEntry(0xFF21,   // Trigger Bank Tag
+			       1 ); // bank num
+
+	    if(!ebi.tagNumExists(tn))
+	      {
+		continue;
+	      }
+
+	    bankIndex bi_trigbank = ebi.getBankIndex(tn);
+	    evioBankIndex bi_triggerbank((uint32_t *)bi_trigbank.bankPointer, 4);
+
+	    evioBankIndex bi_evTypeBank;
+	    for(b_iter = bi_triggerbank.tagNumMap.begin(); b_iter != bi_triggerbank.tagNumMap.end();
+		b_iter++)
+	      {
+		if((b_iter->first.getTag() == 0x2) &&
+		   (b_iter->second.contentType == EVIO_USHORT16))
+		  {
+		    d16 = (uint16_t *)b_iter->second.data;
+		    len = b_iter->second.dataLength;
+		    if (d16 != NULL)
+		      {
+			evType = d16[0];
+		      }
+		  }
+	      }
+
+	    tn = evioDictEntry(0x2,  // Eventnumber/timestamp Bank Tag=0x2
+			       0, // num
+			       0, // tag en
+			       EVIO_ULONG64,
+			       true,
+			       "",
+			       "");
+
+
+	    if(!bi_triggerbank.tagNumExists(tn))
+	      {
+		continue;
+	      }
+
+	    d64 = bi_triggerbank.getData < uint64_t > (tn, &len);
 	    if (d64 != NULL)
 	      {
 		eventNumber = d64[0];
 		eventTimestamp = d64[1];
 	      }
 
-	    tn = evioDictEntry(0x2,	// Event Type bank
-			       0,	// num
-			       0,	// tagend
-			       true,	// hasparent
-			       0xFF21,	// parentTag
-			       1,	// parentNum
-			       0,	// parentTagEnd
-			       EVIO_USHORT16, false, "", "");
-
-	    // Bug in evioBankIndex::getData only checks on entry in the map..
-	    // I guess we gotta do this.
-	    bankIndexMap::const_iterator iter = bi.tagNumMap.find(tn);
-	    while (iter != bi.tagNumMap.end())
-	      {
-		if (((*iter).second).contentType == EVIO_USHORT16)
-		  {
-		    d16 = (const uint16_t *) ((*iter).second).data;
-		    len = ((*iter).second).dataLength;
-		  }
-		iter++;
-	      }
-
-	    if ((d16 != NULL) && (len != 0))
-	      {
-		evType = d16[0];
-	      }
 	  }
 
 	// Check to see if this event falls into the user specified range
@@ -208,16 +236,30 @@ main(int argc, char **argv)
 	    printf("# %6d: evType = %d   eventTimestamp = 0x%lx \n",
 		   (uint32_t) eventNumber, evType, eventTimestamp);
 
+	    int rocnum = 4;
+	    if(rocBiMap.find(rocnum) == rocBiMap.end())
+	      {
+		cout << "#  NO ROC " << rocnum << endl;
+		continue;
+	      }
 
+	    evioBankIndex ebi_roc((uint32_t *)rocBiMap.find(4)->second.bankPointer,0);
 	    // Get the ROC / Payload data
-	    tn = evioDictEntry(mpdtag, mpdnum, 0, true, rocnum,	// ROC number
-			       1, 0, EVIO_UINT32, false, "", "");
+	    int32_t mpdtag = 10, mpdnum = 0;
+	    tn = evioDictEntry(mpdtag, mpdnum);
 
-	    d32 = bi.getData < uint32_t > (tn, &len);
+	    d32 = ebi_roc.getData < uint32_t > (tn, &len);
 
 	    if (d32 != NULL)
 	      {
+		printf("#      ROC %2d MPD+APV Data\n",
+		       rocnum);
 		mdat[rocnum]->DecodeBuffer(d32, len);
+	      }
+	    else
+	      {
+		printf("#      NO ROC %2d d32 !!! \n",
+		       rocnum);
 	      }
 
 	  }
